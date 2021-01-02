29c6c2fe5c5906dbccc3744439b486afe7af74a9
wataoka <wataoka@stu.kobe-u.ac.jp>
2021-01-02 16:47:29 +0900

    change: MATHJAX: 1 in config/env.dev.js

--- config/env.dev.js
+++ config/env.dev.js
@@ -2,7 +2,7 @@ module.exports = {
   NODE_ENV: 'development',
   FILE_UPLOAD: 'mongodb',
   // MONGO_GRIDFS_TOTAL_LIMIT: 10485760,   // 10MB
-  // MATHJAX: 1,
+  MATHJAX: 1,
   // NO_CDN: true,
   ELASTICSEARCH_URI: 'http://localhost:9200/growi',
   HACKMD_URI: 'http://localhost:3010',
